26a1fe6be02220186457ca54b9901be3d1a2bbbb
MauroBraga <mrb0305@gmail.com>
2020-11-20 14:34:06 -0300

    Criando tooltip de erros

--- gobarber-web/src/components/Input/styles.ts
+++ gobarber-web/src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../Tooltip';
+
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -14,17 +17,23 @@ export const Container = styled.div<ContainerProps>`
   color: #666360;
 
   ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+   ` };
+
+  ${props =>
     props.isFocused &&
     css`
       color: #ff9000;
       border-color: #ff9000;
-    `};
+  `};
 
   ${props =>
     props.isFilled &&
     css`
       color: #ff9000;
-    `};
+  `  };
 
   width: 100%;
   display: flex;
@@ -44,5 +53,21 @@ export const Container = styled.div<ContainerProps>`
   }
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16;
+  svg {
+    margin: 0;
+  }
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
