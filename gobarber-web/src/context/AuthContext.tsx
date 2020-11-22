4256554d81d98164a78d16b8c203f9367c19b60f
MauroBraga <mrb0305@gmail.com>
2020-11-22 08:45:47 -0300

    Criando hook useAuth

--- gobarber-web/src/context/AuthContext.tsx
+++ gobarber-web/src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useCallback, useState } from 'react';
+import React, { createContext, useCallback, useContext, useState } from 'react';
 
 import api from '../services/api';
 
@@ -48,4 +48,15 @@ const AuthProvider: React.FC = ({ children }) => {
     </AuthContext.Provider>
   );
 };
-export { AuthContext, AuthProvider };
+
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
