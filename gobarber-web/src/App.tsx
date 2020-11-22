f2945c91a9bec19857c5b0709e5dc176316d108c
MauroBraga <mrb0305@gmail.com>
2020-11-22 13:22:16 -0300

    Estrutura de Toasts

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './context/AuthContext';
+import ToastContainer from './components/ToastContainer';
+import { AuthProvider } from './hooks/AuthContext';
 
 import SignIn from './pages/SignIn/index';
 
@@ -11,6 +12,7 @@ const App: React.FC = () => (
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
