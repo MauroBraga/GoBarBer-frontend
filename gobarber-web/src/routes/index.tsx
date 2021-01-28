b5b19993fe471d104a4a0ee4480ec31639b6f069
MauroBraga <mrb0305@gmail.com>
2021-01-28 18:15:47 -0300

    Criando página de redefinição

--- gobarber-web/src/routes/index.tsx
+++ gobarber-web/src/routes/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Route';
 
+import ResetPassword from '../pages/ResetPassword';
+
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import ForgotPassword from '../pages/ForgotPassword';
@@ -14,6 +16,7 @@ const Routes: React.FC = () => {
       <Route path="/" exact component={SignIn} />
       <Route path="/signup" component={SignUp} />
       <Route path="/forgot-password" component={ForgotPassword} />
+      <Route path="/reset-password" component={ResetPassword} />
       <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
