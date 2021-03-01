388956b23edece873f23b36ac5f6c71120d62f54
Mauro <mrb030584@yahoo.com>
2021-03-01 13:38:52 -0300

    Criando primeiro teste do zero

---
+++ gobarber-web/src/__tests__/pages/SignIn.spect.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import SignIn from  '../../pages/SignIn';
+import { render } from '@testing-library/react';
+
+const mockedHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => {
+  return {
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
+    Link: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
+
+describe('SignIn Page', () => {
+  it('should be able to sign in', () => {
+    const {debug} = render(<SignIn />);
+    debug();
+  });
+});
